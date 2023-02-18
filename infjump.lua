d17a378369c5f2e49d296b72fa187d56f9e04c05
99119240 <105958391+99119240@users.noreply.github.com>
2023-02-17 17:20:49 -0800

    Create infjump.lua

---
+++ infjump.lua
@@ -0,0 +1,18 @@
+local Player = game:GetService'Players'.LocalPlayer;
+local UIS = game:GetService'UserInputService';
+
+_G.JumpHeight = 50;
+
+function Action(Object, Function) if Object ~= nil then Function(Object); end end
+
+UIS.InputBegan:connect(function(UserInput)
+    if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.Space then
+        Action(Player.Character.Humanoid, function(self)
+            if self:GetState() == Enum.HumanoidStateType.Jumping or self:GetState() == Enum.HumanoidStateType.Freefall then
+            
+            wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "INF Jump is enabled ";
+Text = "Anticheat=true"; 
+Duration = 20;
+})
